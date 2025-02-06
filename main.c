c89d16e8d78a1d732dece108b81e252ab9ac85b2
Yerti <36551149+UZ9@users.noreply.github.com>
2025-02-05 20:37:21 -0500

    Add usage, HEX input

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include "libusb-1.0/libusb.h"
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 // G502 ID retrieved via lsusb
 #define VID 0x046d // G502 Vender ID
 #define PID 0xc08b // G502 Product ID
 
-#define G502_LOGO_INTERFACE 0x01
+#define G502_INTERFACE 0x01
 
 // Captured via wireshark, mouse config is sent via SET_REPORT
 // note: endian is reversed
@@ -24,11 +25,11 @@
       printf(fmt, ##__VA_ARGS__);                                              \
   } while (0)
 
-static int set_mouse_color(unsigned char *color) {
-  if (!color) {
-    fprintf(stderr, "ERROR: invalid color provided  to set_mouse_color");
-    return -1;
-  }
+enum G502_LED_ID { LOGO_LED = 0x01, DPI_LED = 0x00 };
+
+static int set_mouse_color(uint8_t interface, uint8_t red, uint8_t blue,
+                           enum G502_LED_ID green) {
+  libusb_init_context(NULL, NULL, 0);
 
   libusb_device_handle *handle;
   libusb_device *device;
@@ -44,10 +45,10 @@ static int set_mouse_color(unsigned char *color) {
   }
 
   // check for kernel driver
-  if (libusb_kernel_driver_active(handle, G502_LOGO_INTERFACE)) {
+  if (libusb_kernel_driver_active(handle, G502_INTERFACE)) {
     LOG_DEBUG("Found active kernel, deactivating...");
 
-    int result = libusb_detach_kernel_driver(handle, G502_LOGO_INTERFACE);
+    int result = libusb_detach_kernel_driver(handle, G502_INTERFACE);
 
     if (result < 0) {
       const char *error_name = libusb_error_name(result);
@@ -57,7 +58,7 @@ static int set_mouse_color(unsigned char *color) {
     }
   }
 
-  int status = libusb_claim_interface(handle, G502_LOGO_INTERFACE);
+  int status = libusb_claim_interface(handle, G502_INTERFACE);
 
   if (status < 0) {
     const char *error_name = libusb_error_name(status);
@@ -67,14 +68,14 @@ static int set_mouse_color(unsigned char *color) {
   }
 
   unsigned char data[] = {
-      0x11,     0xff, 0x02, 0x3c, // g502 magic numbers*/
-      0x01,                       // interface: 0x01 for logo*/
-      0x01,     // config: 0x00 off, 0x01 fixed, 0x02 breathing, 0x03 cycle*/
-      color[0], // red component*/
-      color[1], // blue component*/
-      color[2], // green component*/
-      0x02,     0x00, 0x00, // fixed config*/
-      0x00,     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+      0x11,      0xff, 0x02, 0x3c, // g502 magic numbers*/
+      interface,                   // interface: 0x01 for logo*/
+      0x01,  // config: 0x00 off, 0x01 fixed, 0x02 breathing, 0x03 cycle*/
+      red,   // red component*/
+      blue,  // blue component*/
+      green, // green component*/
+      0x02,      0x00, 0x00, // fixed config*/
+      0x00,      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
   // at this point, we have the interface: let's add a solid color
   int result =
@@ -84,8 +85,8 @@ static int set_mouse_color(unsigned char *color) {
   if (result < 0) {
     const char *error_name = libusb_error_name(result);
     fprintf(stderr, "ERROR: %d %s while sending packet", result, error_name);
-    libusb_release_interface(handle, G502_LOGO_INTERFACE);
-    libusb_attach_kernel_driver(handle, G502_LOGO_INTERFACE);
+    libusb_release_interface(handle, G502_INTERFACE);
+    libusb_attach_kernel_driver(handle, G502_INTERFACE);
     libusb_close(handle);
     libusb_exit(NULL);
 
@@ -94,11 +95,11 @@ static int set_mouse_color(unsigned char *color) {
 
   LOG_DEBUG("Closing device...\n");
 
-  libusb_release_interface(handle, G502_LOGO_INTERFACE);
+  libusb_release_interface(handle, G502_INTERFACE);
 
   // re-enable the kernel--this is quite important, as I accidentally lost all
   // peripherals from forgetting this
-  libusb_attach_kernel_driver(handle, G502_LOGO_INTERFACE);
+  libusb_attach_kernel_driver(handle, G502_INTERFACE);
 
   libusb_close(handle);
   libusb_exit(NULL);
@@ -106,28 +107,81 @@ static int set_mouse_color(unsigned char *color) {
   return 0;
 }
 
+void printUsage() {
+  printf("USAGE:\n");
+  printf("  g502-controller [command] <options>\n");
+  printf("\n");
+  printf("COMMANDS:\n");
+  printf("  %-20s %s\n", "logo-light <hex>",
+         "Sets the color of the Logitech LED Logo");
+  printf("  %-20s %s\n", "dpi-light <hex>", "Sets the color of the DPI lights");
+}
+
+int validateHex(char *hex) {
+  // check for 0x
+
+  int strLen = strlen(hex);
+
+  // valid formats: 0xRRGGBB, 000000
+  if (strLen != 6 && strLen != 8) {
+    return -1;
+  }
+
+  int start = 0;
+
+  // check for valid prefix
+  if (strLen == 8) {
+    if (hex[0] != '0' || hex[1] != 'x') {
+      return -1;
+    }
+
+    start = 2;
+  }
+
+  for (int i = start; i < strLen; i++) {
+    // check each character for valid format
+    if (!(hex[i] >= '0' && hex[i] <= '9' || hex[i] >= 'A' && hex[i] <= 'F' ||
+          hex[i] >= 'a' && hex[i] <= 'f')) {
+      return -1;
+    }
+  }
+
+  return 0;
+}
+
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    fprintf(stderr, "USAGE: g502-light-controller <failed|passed>\n");
+  if (argc != 3) {
+    printUsage();
     return 0;
   }
 
   char *input = argv[1];
   int passed = 0;
 
-  if (strcmp(input, "failed") == 0) {
-    passed = 0;
-  } else if (strcmp(input, "passed") == 0) {
-    passed = 1;
+  if (strcmp(input, "logo-light") == 0) {
+    if (validateHex(argv[2]) == -1) {
+      printf("ERROR: Invalid hexcode provided: %s", argv[2]);
+      return 0;
+    }
+
+    int hex = (int)strtol(argv[2], NULL, 16);
+
+    set_mouse_color(LOGO_LED, (hex & 0xFF0000) >> 16, (hex & 0xFF00) >> 8,
+                    hex & 0xFF);
+  } else if (strcmp(input, "dpi-light") == 0) {
+    if (validateHex(argv[2]) == -1) {
+      printf("ERROR: Invalid hexcode provided: %s", argv[2]);
+      return 0;
+    }
+
+    int hex = (int)strtol(argv[2], NULL, 16);
+
+    set_mouse_color(DPI_LED, (hex & 0xFF0000) >> 16, (hex & 0xFF00) >> 8,
+                    hex & 0xFF);
   } else {
-    fprintf(stderr, "USAGE: g502-light-controller <failed|passed>\n");
+    printUsage();
     return -1;
   }
 
-  libusb_init_context(NULL, NULL, 0);
-
-  unsigned char passedColor[] = {0x00, 0xFF, 0x00};
-  unsigned char failedColor[] = {0xFF, 0x00, 0x00};
-
-  return set_mouse_color(passed ? passedColor : failedColor);
+  return 0;
 }
